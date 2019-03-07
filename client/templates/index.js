0c05e9561e7b8bca8e6bfe78d90bd64b1c51aca1
AkshayViru <akshayknr7@gmail.com>
2019-03-07 22:32:20 +0530

    meta auth

--- client/templates/index.js
+++ client/templates/index.js
@@ -6,7 +6,9 @@ Template.index.events({
     var illegalChars = /\W/;
 
     var textbox = $("[data-id=username]").val();
-    console.log(textbox);
+
+    let user_address = web3.eth.defaultAccount; 
+
     if(textbox.length < 3){
         Bert.alert("Username must have a minimum of 3 characters");
     }
@@ -14,45 +16,89 @@ Template.index.events({
         Bert.alert("Username can contain only characters, numbers, and underscore");
     }
     else{
-      let user_address = web3.eth.defaultAccount;  
-      UserCrt.getUsernameByAddress('0x25E8f77c46aeB120F31EB09ef119724C2B80a703',function(error, result){
-        if(!error)
-            {
-              var str = web3.toAscii(result);
-              console.log(str);
+
+      UserCrt.getUsernameByAddress(user_address ,function(error, result){
+        console.log(result);
+        if(result !== "0x")
+            {           
+              UserCrt.getAddressByUsername(textbox,function(error2, result2){
+                if(result2 !== "0x"){
+                      if(result2 == user_address){
+                        Meteor.call('user.generateNewLoginAttempt', user_address, textbox, function(err, nonce) {
+                          // nonce = the nonce generated on server
+                          console.log('nonce: ' + nonce);
+                          // hash nonce
+                          let sh3_nonce = web3.sha3(nonce); // depending on web3 version use: web3.utils.sha3(nonce)
+                          console.log('sh3 hashed nonce: ' + sh3_nonce);
+                    
+                          // sign hashed nonce
+                          web3.eth.sign(user_address, sh3_nonce, function(err, res) {
+                            if (err) {
+                              console.log(err);
+                            } else {
+                              // res = the signed nonce
+                              // login with signed nonce
+      
+                              login(res, textbox, nonce);
+      
+                            }
+                          });
+                    
+                        });
+                      }
+                      else{
+                          Bert.alert("Username and public key don't match1");
+                      }
+                }
+                else{
+                  Bert.alert("Username and public key don't match2");
+                }
+              });
+
             }
-        else
-            console.error(error);
-      });
-     /* UserCrt.createUser(textbox, "none",function(error, result){
-        if(!error)
-            {
-              console.log("added");
+        else{
+          UserCrt.usernameTaken(textbox, function(error2, result2){
+            if(result2 !== true){
+                  UserCrt.createUser(textbox, "none",function(error2, result2){
+                    if(!error2)
+                        {
+                          console.log("added");
+                        }
+                    else
+                        console.error(error2);
+                  });
+
+                  Meteor.call('user.generateNewLoginAttempt', user_address, textbox, function(err, nonce) {
+                    // nonce = the nonce generated on server
+                    console.log('nonce: ' + nonce);
+                    // hash nonce
+                    let sh3_nonce = web3.sha3(nonce); // depending on web3 version use: web3.utils.sha3(nonce)
+                    console.log('sh3 hashed nonce: ' + sh3_nonce);
+              
+                    // sign hashed nonce
+                    web3.eth.sign(user_address, sh3_nonce, function(err, res) {
+                      if (err) {
+                        console.log(err);
+                      } else {
+                        // res = the signed nonce
+                        // login with signed nonce
+
+                        login(res, textbox, nonce);
+
+                      }
+                    });
+              
+                  });
             }
-        else
-            console.error(error);
-      });*/
-  
-      Meteor.call('user.generateNewLoginAttempt', user_address, textbox, function(err, nonce) {
-        // nonce = the nonce generated on server
-        console.log('nonce: ' + nonce);
-        // hash nonce
-        let sh3_nonce = web3.sha3(nonce); // depending on web3 version use: web3.utils.sha3(nonce)
-        console.log('sh3 hashed nonce: ' + sh3_nonce);
-  
-        // sign hashed nonce
-        web3.eth.sign(user_address, sh3_nonce, function(err, res) {
-          if (err) {
-            console.log(err);
-          } else {
-            // res = the signed nonce
-            // login with signed nonce
-            login(res);
-          }
-        });
-  
+            else{
+              Bert.alert("Username already exists");
+            }
+          });
+        }
       });
     }
+
+    
   },
   'keyup [data-id=username]': (event, template) => {
     // If body section has text enable the submit button, else disable it
@@ -75,16 +121,24 @@ Template.index.onRendered(() => {
   $("#LoginButtonMM").prop('disabled', true);
 });
 
-function login(signed_nonce) {
-  let user_address = web3.eth.defaultAccount;
+function login(signed_nonce, username, nonce) {
 
-  Meteor.call('user.login', user_address, signed_nonce, function(err, res) {
-    if (err) {
-      console.log(err);
-    } else {
-      //Meteor.loginWithToken(stampedLoginToken.token);
-      Meteor.loginWithToken(res.token);
-      $('#loginModal').modal('hide');
-    }
+  UserCrt.getAddressByUsername(username, function(error, result){
+    if(!error)
+        {
+          Meteor.call('user.login', result, signed_nonce, nonce, function(err, res) {
+            if (err) {
+              console.log(err);
+            } else {
+              //Meteor.loginWithToken(stampedLoginToken.token);
+              Meteor.loginWithToken(res.token);
+              console.log(res.token)
+              $('#loginModal').modal('hide');
+            }
+          });
+        }
+    else
+        console.error(error);
   });
+
 }
--- server/methods/login.js
+++ server/methods/login.js
@@ -7,71 +7,45 @@ var web3 = new Web3('');
 
 Meteor.methods({
   'user.generateNewLoginAttempt': function(user_address, textbox) {
-    let exists = Meteor.users.findOne({username: user_address});
     let nonce = Random.secret(16);
-
-    if (exists) {
-      // user already exists, update nonce
-      Meteor.users.update(exists._id, {$set: {nonce: nonce}});
-    } else {
-      // create new user
-     /* let userId = Accounts.createUser({
-        username: user_address,
-        address: user_address,
-        nonce: nonce
-      });
-      var buf = Buffer.from(textbox+"="+user_address);
-
-      ipfs.files.add(buf, (error, result) => {
-        if(error) {
-          console.error(error)
-          return
-        }
-        //this.simpleStorageInstance.set(result[0].hash, { from: this.state.account }).then((r) => {
-          console.log('ifpsHash', result);
-        });*/
-      }
-
     return nonce;
   },
-  'user.login': function(user_address, signed_nonce) {
-    let user = Meteor.users.findOne({username: user_address});
+  'user.login': function(user_address, signed_nonce, nonce) {
+    
+        //for generating user id necessary for login
+        let user = Meteor.users.findOne({username: web3.utils.toChecksumAddress(user_address)});
+        
+        if(!user){
+          Accounts.createUser({"username": user_address});
+        }
+      
+        let sh3_nonce = web3.utils.sha3(nonce);
 
-    if (user) {
-      // hash the nonce retrieved from database
-      let sh3_nonce = web3.utils.sha3(user.nonce);
+        // https://web3js.readthedocs.io/en/1.0/web3-eth-accounts.html#recover
+        let v = '0x' + signed_nonce.slice(130,134);
+        let r = signed_nonce.slice(0,66);
+        let s = '0x' + signed_nonce.slice(66,130);
 
-      // https://web3js.readthedocs.io/en/1.0/web3-eth-accounts.html#recover
-      let v = '0x' + signed_nonce.slice(130,134);
-      let r = signed_nonce.slice(0,66);
-      let s = '0x' + signed_nonce.slice(66,130);
-
-      // recover public key from hashed nonce
-      let public_key = web3.eth.accounts.recover({
-        messageHash: sh3_nonce,
-        v: v,
-        r: r,
-        s: s
-      });
-
-      // make sure we have both addresses in the same format
-      public_key    = web3.utils.toChecksumAddress(public_key);
-      user.username = web3.utils.toChecksumAddress(user.username);
-
-      // check if users public key matches the address
-      // of the private key that was used to hash the nonce
-      if (public_key === user.username) {
-        // success, now login user
-        let stampedLoginToken = Accounts._generateStampedLoginToken();
-        Accounts._insertLoginToken(user._id, stampedLoginToken);
-        console.log('Success! Returning login token!');
-        return stampedLoginToken;
-      } else {
-        throw new Meteor.Error('Login attempt not valid');
-      }
-    } else {
-      // user with address not found
-      throw new Meteor.Error('Login attempt not valid');
+        // recover public key from hashed nonce
+        let public_key = web3.eth.accounts.recover({
+          messageHash: sh3_nonce,
+          v: v,
+          r: r,
+          s: s
+        });
+        // make sure we have both addresses in the same format
+        public_key    = web3.utils.toChecksumAddress(public_key);
+        user_address  = web3.utils.toChecksumAddress(user_address);
+        // check if users public key matches the address
+        // of the private key that was used to hash the nonce
+        if (public_key === user_address) {
+          // success, now login user
+          let stampedLoginToken = Accounts._generateStampedLoginToken();
+          Accounts._insertLoginToken(user._id, stampedLoginToken);
+          console.log('Success! Returning login token!');
+          return stampedLoginToken;
+        } else {
+          throw new Meteor.Error('Login attempt not valid');
+        }
     }
-  }
 });
